ba869e585b9ae8aa1b776d164cd4d2036711760a
Aml Moawad <152315852+amlmoawad74@users.noreply.github.com>
2024-03-19 19:26:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,16 @@ import numpy as np
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from flask import Flask, request, render_template, jsonify
 from werkzeug.utils import secure_filename
-from PIL import Image
 
 app = Flask(__name__)
 
 # Load the model
-model_path = 'fruit.h5'
+model_path = 'MyModel.keras'  # Adjust the path to your model file
 try:
     model = tf.keras.models.load_model(model_path)
     model.summary()  # Print model summary for debugging
 except Exception as e:
     print("Error loading the model:", e)
-
 
 print('Model loaded. Check http://127.0.0.1:5000/')
 
@@ -59,7 +57,7 @@ labels = {
     35: 'watermelon'
 }
 
-
+# Function to preprocess the image and get predictions
 def getResult(image_path):
     img = load_img(image_path, target_size=(224, 224))  # Assuming input shape is (224, 224)
     x = img_to_array(img)
@@ -69,11 +67,10 @@ def getResult(image_path):
     predictions = model.predict(x)
     return predictions[0]
 
-
+# Routes
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
-
 
 @app.route('/predictApi', methods=["POST"])
 def api():
@@ -84,37 +81,24 @@ def api():
         basepath = os.path.dirname(__file__)
         file_path = os.path.join(basepath, 'uploads', secure_filename(image.filename))
         image.save(file_path)
-        print("0")
         predictions = getResult(file_path)
-        print("1")
         predicted_label = labels[np.argmax(predictions)]
-        print("2")
         return jsonify({'prediction': predicted_label})
     except Exception as e:
-        print("3")
         return jsonify({'Error': str(e)})
-
 
 @app.route('/predict', methods=['POST'])
 def upload():
     if request.method == 'POST':
-        print("4")
         f = request.files['file']
-        print("5")
         basepath = os.path.dirname(__file__)
-        print("6")
         file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
-        print("7")
         f.save(file_path)
-        print("8")
         predictions = getResult(file_path)
-        print("9")
         predicted_label = labels[np.argmax(predictions)]
-        print("10")
         return predicted_label
 
     return None
-
 
 if __name__ == '__main__':
     app.run(debug=True)
